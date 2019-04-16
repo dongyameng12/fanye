b59804de6f5bfed71e7cbacf45191092dd1f4d30
yangtao <1509440167@qq.com>
2019-04-17 00:20:12 +0800

    111

--- js/index.js
+++ js/index.js
@@ -11,17 +11,74 @@
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
 $(function () {
-    var audio = document.getElementById('music')
-    audio.pause()
-    $('#musicMenu').on('click',function () {
-        if (!$(this).hasClass('move')) {
-            $(this).addClass('move')
-            audio.play()
-        } else {
-            $(this).removeClass('move')
-            audio.pause()
+    var music = document.getElementById('music'),
+        ms1 = document.getElementById('ms1'),
+        ms2 = document.getElementById('ms2'),
+        ms3 = document.getElementById('ms3'),
+        ms4 = document.getElementById('ms4'),
+        ms5 = document.getElementById('ms5');
+    music.load();
+    ms1.load();
+    ms2.load();
+    ms3.load();
+    ms4.load();
+    ms5.load();
+    //bgMusic
+    wx.config({
+        debug: false
+    });
+    wx.ready(function () {
+        function audioAutoPlay(id){
+            var audio = document.getElementById(id),
+                play = function(){
+                    audio.play();
+                    audio.pause();
+                    document.removeEventListener("touchstart",play, false);
+                };
+            audio.play();
+            audio.pause();
+            document.addEventListener("WeixinJSBridgeReady", function () {
+                play();
+                music.play();
+            }, false);
+            document.addEventListener('YixinJSBridgeReady', function() {
+                play();
+                music.play();
+            }, false);
+            document.addEventListener("touchstart",play, false);
         }
-    })
+        music.play();
+        audioAutoPlay('ms1');
+        audioAutoPlay('ms2');
+        audioAutoPlay('ms3');
+        audioAutoPlay('ms4');
+        audioAutoPlay('ms5');
+    });
+    ~function () {
+        var musicMenu = document.getElementById('musicMenu'),
+            musicAudio = document.getElementById('music');
+
+        musicMenu.addEventListener('click', function () {
+            if (musicAudio.paused) {//->暂停
+                musicAudio.play();
+                musicMenu.className = 'music move';
+                return;
+            }
+            musicAudio.pause();
+            musicMenu.className = 'music';
+        }, false);
+
+        function controlMusic() {
+            musicAudio.volume = 0.5;
+            musicAudio.pause();
+            musicAudio.addEventListener('canplay', function () {
+                musicMenu.style.display = 'block';
+                //console.log(1);
+                musicMenu.className = 'music move';
+            }, false);
+        }
+        window.setTimeout(controlMusic, 1000);
+    }();
 })
 
 //显示遮罩层
